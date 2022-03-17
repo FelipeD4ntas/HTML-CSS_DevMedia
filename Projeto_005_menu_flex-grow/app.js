7f978538d5bab756c0f1486deaadf7e054850a39
Marcos Felipe Dantas da Costa <felipeicedantas@gmail.com>
2022-03-17 09:46:49 -0300

    up

--- Projeto_005_menu_flex-grow/app.js
+++ Projeto_005_menu_flex-grow/app.js
@@ -1,4 +1,7 @@
 const circulo = document.querySelector('#circulo-um');
+const cards = document.querySelectorAll('.cards');
+const cont_cards = document.querySelectorAll('.cont-card');
+
 window.addEventListener('wheel', girou);
 
 function girou(event) {
@@ -9,4 +12,41 @@ function girou(event) {
     } else {
         circulo.style.transform = `rotate(-90deg)`;
     }
+}
+
+
+cont_cards[0].addEventListener('mouseenter', entrou_um);
+cont_cards[1].addEventListener('mouseenter', entrou_dois);
+cont_cards[2].addEventListener('mouseenter', entrou_tres);
+cont_cards[3].addEventListener('mouseenter', entrou_quatro);
+
+cont_cards[0].addEventListener('mouseleave', saiu_um);
+cont_cards[1].addEventListener('mouseleave', saiu_dois);
+cont_cards[2].addEventListener('mouseleave', saiu_tres);
+cont_cards[3].addEventListener('mouseleave', saiu_quatro);
+
+function entrou_um() {
+    cards[0].style.transform = 'rotateY(180deg)';
+}
+function entrou_dois() {
+    cards[1].style.transform = 'rotateY(180deg)';
+}
+function entrou_tres() {
+    cards[2].style.transform = 'rotateY(180deg)';
+}
+function entrou_quatro() {
+    cards[3].style.transform = 'rotateY(180deg)';
+}
+
+function saiu_um() {
+    cards[0].style.transform = 'rotateY(0deg)';
+}
+function saiu_dois() {
+    cards[1].style.transform = 'rotateY(0deg)';
+}
+function saiu_tres() {
+    cards[2].style.transform = 'rotateY(0deg)';
+}
+function saiu_quatro() {
+    cards[3].style.transform = 'rotateY(0deg)';
 }
