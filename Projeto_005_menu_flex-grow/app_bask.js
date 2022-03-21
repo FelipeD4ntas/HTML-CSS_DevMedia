7b06f616fc5992de181cf2e489f76fb768edb955
Marcos Felipe Dantas da Costa <felipeicedantas@gmail.com>
2022-03-21 13:10:59 -0300

    up
    Finalizado a pág do basquete

--- Projeto_005_menu_flex-grow/app_bask.js
+++ Projeto_005_menu_flex-grow/app_bask.js
@@ -2,7 +2,7 @@ const circulo = document.querySelector('#circulo');
 
 window.addEventListener('wheel', girou);
 
-// Função para girar os circulos da página de futebol.
+// Função para girar os circulos da página de basquete.
 function girou(event) {
     if (event.wheelDelta < 0) {
         circulo.style.transform = `rotate(90deg)`;
